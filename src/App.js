6f5f5f341f509fea848016a03f02325838b7553c
PokizaSdv <pokizasdv@gmail.com>
2023-12-15 15:32:39 -0500

    Implement Adding new Todo and Render it as List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,96 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 
-class  App extends React.Component { 
-  render() {
-    return (
-     <main></main>
-  )
-  }
-  
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [],
+            inputVaLue: "",
+            inputError: false,
+            showEditModal: false,
+            editingInputValue: "",
+            editingTodoId: ""
+        };
+    }
+
+    addTodo = (e) => {
+        e.preventDefault();
+        if (this.state.inputVaLue.length <= 2) {
+            this.setState({
+                inputError: true
+            });
+            return;
+        }
+        const newTodo = {
+            id: uuid(),
+            text: this.state.inputVaLue,
+            isDone: false
+        };
+
+        this.setState((prevState) => {
+            const copyTodos = [...prevState.todos, newTodo];
+            return {
+                todos: copyTodos,
+                inputVaLue: ""
+            };
+        });
+    };
+
+    handleOnChange = (e) => {
+        const { value } = e.target;
+        this.setState({
+            inputVaLue: value
+        });
+        if (value.length <= 2) {
+            this.setState({
+                inputError: true
+            });
+        } else {
+            this.setState({
+                inputError: false
+            });
+        }
+    };
+
+    
+
+    render() {
+        return (
+            <main>
+                <form onSubmit={this.addTodo}>
+                    <div className="form-control">
+                        <input
+                            className="input"
+                            onChange={this.handleOnChange}
+                            value={this.state.inputVaLue}
+                            type="text"
+                            placeholder="What is your todo?"
+                        ></input>
+                        {this.state.inputError && <span>Invalid Todo</span>}
+                    </div>
+                    <input type="submit" value="Add todo"></input>
+                </form>
+                <ul>
+                    {this.state.todos.map((todo) => {
+                        return (
+                            <li
+                                key={todo.id}
+                                className={todo.isDone ? "todo-done" : ""}
+                            >
+                                <span className="todo-text">{todo.text}</span>
+
+                                
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                
+            </main>
+        );
+    }
 }
 
 export default App;
